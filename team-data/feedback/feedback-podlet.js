14c3839b672406828eaace8a84bae0d99e335e09
Jens-Christian Bjerkek <jens-christian.bjerkek@sparebank1.no>
2021-05-04 21:37:00 +0200

    Adding a Vue podlet

--- team-data/feedback/feedback-podlet.js
+++ team-data/feedback/feedback-podlet.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Podlet = require('@podium/podlet');
+const utils = require('@podium/utils');
+const fs = require('fs');
 
 const app = express();
 
@@ -17,16 +19,48 @@ const podlet = new Podlet({
     development: true,
 });
 
+podlet.view((incoming, content) => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            ${incoming.css.map(utils.buildLinkElement).join('\n')}
+            <title>${incoming.view.title}</title>
+        </head>
+        <body>
+            ${content}
+            ${incoming.js.map(utils.buildScriptElement).join('\n')}
+        </body>
+    </html>`;
+});
+
+
 app.use(podlet.middleware());
 
 app.get(podlet.content(), (req, res) => {
     res.status(200).podiumSend(`
-        <div>This is the feedback podlet</div>
+        <div id="feedbackPodlet"></div>
     `);
 });
 
 app.get(podlet.manifest(), (req, res) => {
     res.status(200).send(podlet);
+});
+
+const assetsFolder = 'dist'
+app.use('/assets', express.static(assetsFolder));
+
+fs.readdir(`${assetsFolder}/css`, (err, files) => {
+    files.forEach(filename => {
+        podlet.css({ value: `${url}/assets/css/${filename}` });
+    });
+});
+
+fs.readdir(`${assetsFolder}/js`, (err, files) => {
+    files.forEach(filename => {
+        podlet.js({ value: `${url}/assets/js/${filename}` });
+    });
 });
 
 app.listen(port, () => {
--- team-loans/layout/loans-layout.js
+++ team-loans/layout/loans-layout.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const Layout = require('@podium/layout');
 const utils = require('@podium/utils');
-const template = require('../../template');
 
 const app = express();
 
